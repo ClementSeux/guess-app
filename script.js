74f783e015aeaae03998a5d1e9614ab5a05e5300
Clement Seux <clement.seux@gmail.com>
2024-05-14 14:35:00 +0200

    feat: add script

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // event listener on #burger-button to toggle class 'open' on #burger-menu
 document.getElementById("burger-button").addEventListener("click", function () {
     document.getElementById("burger-menu").classList.toggle("open");
+    document.getElementById("burger-button").classList.toggle("open");
+});
+
+// :event listener on input #guess add a treaming underscore after each keypress
+document.getElementById("guess").addEventListener("input", function () {
+    document.getElementById("input-display").innerHTML = this.value + "_";
 });
