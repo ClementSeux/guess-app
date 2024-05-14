16ca82b37d06ced0ebd3e4b75ad980fc11a6d560
Clement Seux <clement.seux@gmail.com>
2024-05-14 14:34:38 +0200

    feat: add grid

---
+++ script.js
@@ -0,0 +1,4 @@
+// event listener on #burger-button to toggle class 'open' on #burger-menu
+document.getElementById("burger-button").addEventListener("click", function () {
+    document.getElementById("burger-menu").classList.toggle("open");
+});
